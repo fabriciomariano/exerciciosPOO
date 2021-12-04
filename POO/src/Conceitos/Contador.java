37016288c86740264cc2cd9af016902229ffb179
Fabricio Mariano <fabriciomariano@gmail.com>
2021-12-03 22:08:19 -0300

    "Exercícios POO Java"
    Change-Id: I0ae4da41a84c88ddc792b373129a99913b1304b2

---
+++ POO/src/Conceitos/Contador.java
@@ -0,0 +1,19 @@
+package Conceitos;
+
+public class Contador {
+	
+	public static void main(String[] args){
+	    
+		String str = "";
+		int vezes = 100;
+	      
+	     for(int i = 0; i <= vezes; i++) {
+	    	 str +="\nDesculpe";
+	     }
+	    	 System.out.println(str);
+	    	 
+	     }
+	    
+
+}
+
